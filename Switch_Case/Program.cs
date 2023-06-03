3e305b4b9a41056632756c22a96dc5ef6f9bcd47
Siiixxx <jonathan.carllos1994@gmail.com>
2023-06-03 14:27:44 -0300

    Adicionar arquivos de projeto.

---
+++ Switch_Case/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+        string dia;
+
+        switch (x)
+        {
+            case 1:
+                dia = "Domingo";
+                break;
+            case 2:
+                dia = "Segunda-feira";
+                break;
+            case 3:
+                dia = "Terça_feira";
+                break;
+            case 4:
+                dia = "Quarta-feira";
+                break;
+            case 5:
+                dia = "Quinta-feira";
+                break;
+            case 6:
+                dia = "Sexta-feira";
+                break;
+            case 7:
+                dia = "Sábado";
+                break;
+            default:
+                dia = "Valor inválido";
+                break;
+        }
+        Console.WriteLine("Dia da semana: " + dia);
+
+    }
+}
